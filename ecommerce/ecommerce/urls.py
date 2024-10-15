c1da3a871e0fdd4ab65b97e160232310217bfcf2
olani siyum <olisiyum@gmail.com>
2024-10-15 14:59:27 +0300

    Updated database settings for PythonAnywhere

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('products.urls')),  # Include the product's urls
+    path('api/', include('products.urls')),
 ]
